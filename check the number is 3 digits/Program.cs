0ebec4efc25644831a9e2a4be67168c7f53a9be6
SamirAliyevv <samirsa@code.edu.az>
2023-01-16 22:30:12 -0800

    master

---
+++ check the number is 3 digits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace check_the_number_is_3_digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 99, 151, 999, 45, 354, 234 };
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]>99 && numbers[i]<1000)
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine(count);
+
+
+
+        }
+    }
+}
